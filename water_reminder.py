4cf6acf973fa293f654ed35163a54b4bc6e36e04
Vivek <vy2186047@gmail.com>
2026-07-09 20:43:29 +0530

    initial commit

---
+++ water_reminder.py
@@ -0,0 +1,9 @@
+import time
+
+from plyer import notification
+
+while True:
+    print("Please sip some water111")
+    notification.notify(title= "Hey love! Please drink some water and relax for a minute",
+                        message = "i love you",)
+    timesleep(60*60)#apply dot between times and sleep to run this problem
